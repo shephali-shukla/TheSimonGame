fdb8c4d307c8222329dbdd331b0f926f29e2350d
Shephali Shukla <shephalishukla1@gmail.com>
2023-02-26 23:06:19 +0530

    Creating New Pattern

--- game.js
+++ game.js
@@ -2,17 +2,19 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+
+//Creating New Pattern:
 function nextSequence(){
+
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColours[randomNumber];
-
-    gamePattern.push(randomChosenColor);
+    gamePattern.push(randomChosenColor);   
 }
 
-$("#randomChosenColor").click(function(){
 
-    var colorSound = "sounds/" + randomChosenColor + ".mp3";
-    var audio = new Audio(colorSound);
-    $(this).fadeOut(100).fadeIn(100).audio.play();
 
-});
+
+ 
+
+    
+
